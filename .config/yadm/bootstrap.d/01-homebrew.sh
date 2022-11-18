87748b3aae2543a458ef9cc8c47a4683c1ed8a61
DaJiang Zuo <admin@microlove.me>
2022-11-18 09:52:50 +0800

    feat: update

--- .config/yadm/bootstrap.d/01-homebrew.sh
+++ .config/yadm/bootstrap.d/01-homebrew.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-system_type=$(uname -s)
-
-# install homebrew if it's missing
 if ! command -v brew >/dev/null 2>&1; then
   echo "Installing homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
   echo "Setup homebrew"
   echo ''                                               >> ${HOME}/.zshrc
   echo '# Homebrew'                                     >> ${HOME}/.zshrc
@@ -14,14 +10,48 @@ if ! command -v brew >/dev/null 2>&1; then
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'      >> ${HOME}/.zshrc
 fi
 
-# homebrew bundle
-if [ -f "$HOME/.Brewfile" ]; then
-  echo "Updating homebrew bundle"
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-  brew bundle --global
-fi
+echo "Homebrew Taps"
+brew tap homebrew/aliases
+brew tap homebrew/bundle
+brew tap homebrew/cask
+brew tap homebrew/cask-drivers
+brew tap homebrew/cask-fonts
+brew tap homebrew/cask-fonts
+brew tap homebrew/core
+brew tap homebrew/services
+brew tap hashicorp/tap
+brew tap openresty/brew
+brew tap shivammathur/php
 
-# System specific application
+
+echo "Updating homebrew bundle"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+brew install \
+  helm \
+  kubectl \
+  kubectx \
+  cilium-cli \
+  vim \
+  neovim \
+  yadm \
+  pcre \
+  n \
+  nvm \
+  wget \
+  curl \
+  git \
+  lrzsz \
+  htop \
+  telnet \
+  neofetch \
+  tree \
+  jq \
+  make \
+  mysql-client \
+  tmux
+
+
+system_type=$(uname -s)
 if [ "$system_type" = "Darwin" ]; then
   echo "Install macOS applications"
   brew install --cask \
@@ -40,4 +70,8 @@ if [ "$system_type" = "Darwin" ]; then
     charles
 elif [ "$system_type" = "Linux" ]
   echo "Install Linux applications"
+  brew install \
+    zsh \
+    zsh-completions \
+    zsh-git-prompt
 fi
